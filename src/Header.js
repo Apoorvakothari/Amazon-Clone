b5c7a0fe66b94013961c4b5197d9bd36341acc5e
Apoorva Kothari <lit2021029@iiitl.ac.in>
2023-10-04 23:52:20 +0530

    changedIcons

---
+++ src/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Header.css";
+import { AiOutlineSearch } from "react-icons/ai"
+import { BsBasket3Fill } from "react-icons/bs"
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="logo"
+      />
+
+      <div className="header__search">
+        <input className="header__searchInput" type="text"/>
+        <AiOutlineSearch className="header__searchIcon"/>
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option"> 
+            <span 
+            className="header__optionLineOne">Hello Guest</span>
+            <span 
+            className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option"><span 
+            className="header__optionLineOne">Returns</span>
+            <span 
+            className="header__optionLineTwo">& Orders</span> 
+        </div>
+        <div className="header__option"> 
+            <span 
+            className="header__optionLineOne">Your</span>
+            <span 
+            className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+        <BsBasket3Fill> 
+            <span className="header__optionLineTwo header__basketCount">0</span></BsBasket3Fill> 
+
+            {/* CHECK WHY '0' IS NOT SHOWING ON THE HEADER */}
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBZtCAurDXLynt-x1OTH8J-bm4aNrbZTjk",
+    authDomain: "clone-2clone.firebaseapp.com",
+    projectId: "clone-2clone",
+    storageBucket: "clone-2clone.appspot.com",
+    messagingSenderId: "259111123072",
+    appId: "1:259111123072:web:06f7b530915122f3dc0e65",
+    measurementId: "G-NEDMSWRX0K"
+  };
